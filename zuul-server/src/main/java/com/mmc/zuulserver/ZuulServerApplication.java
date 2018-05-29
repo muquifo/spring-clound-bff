1b95c20bcf52b302c0c510c1e0118248918cdfbe
Murilo Carvalho <murilo.carvalho@concrete.com.br>
2018-05-29 15:31:49 -0300

    criado zuul-service

--- zuul-server/src/main/java/com/mmc/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/mmc/zuulserver/ZuulServerApplication.java
@@ -2,11 +2,21 @@ package com.mmc.zuulserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+import com.mmc.zuulserver.filter.SimpleFilter;
+
+@EnableZuulProxy
 @SpringBootApplication
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+	
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 }
