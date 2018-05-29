a4743af393a3ae1beb6048ab0343268e7364e2b3
Murilo Carvalho <murilo.carvalho@concrete.com.br>
2018-05-29 15:57:48 -0300

    organizado arquivos

---
+++ eureka-server-bff/src/main/java/com/mmc/eurekaserver/EurekaServerBffApplication.java
@@ -0,0 +1,12 @@
+package com.mmc.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerBffApplication.class, args);
+	}
+}
